9e777973a9c88faa80e81b831b0fdc55e23a8102
Arsh <lucifer78908@gmail.com>
2022-06-09 20:25:08 +0530

    Prerelease 8 - Bump version to 8
    Signed-off-by: Arsh <lucifer78908@gmail.com>

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         ],
         include=["anai.*", "anai"],
     ),
-    version="prerelease-7",
+    version="prerelease-8",
     license="Apache License 2.0",
     description="Automated ML",
     long_description=long_description,
